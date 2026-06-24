8abdc7ff697f51719eb2e4234c8b5bc80725cbb0
funcieqDEV <166367015+funcieqDEV@users.noreply.github.com>
2026-06-24 12:09:16 +0200

    feat: make Token has SourceSpan

--- Src/Lexer/Token.cs
+++ Src/Lexer/Token.cs
@@ -1,9 +1,10 @@
 namespace Src.Lexer;
-
+using Src.Diagnostics;
 public class Token
 {
     public TokenType Type { get; set; }
     public string Value { get; set; }
+    public SourceSpan Span { get; set; }
 
     public Token(TokenType type, string value)
     {
