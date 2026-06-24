eb73abcbeade3ebe81a5a2f163a7d00c08074358
funcieqDEV <166367015+funcieqDEV@users.noreply.github.com>
2026-06-24 11:59:51 +0200

    feat: add token types

---
+++ Src/Lexer/TokenType.cs
@@ -0,0 +1,45 @@
+namespace Src.Lexer;
+
+public enum TokenType
+{
+    //keyword
+    KExt,
+    KFn,
+    KLet,
+    KIf,
+    KElse,
+    KFor,
+    KRet,
+    KContinue,
+    KBreak,
+
+    //symbols
+    SemiColon,
+    Colon,
+    Comma,
+    LParen,
+    RParen,
+    LBrace,
+    RBrace,
+    LBracket,
+    RBracket,
+    Arrow,
+
+    //operators
+    Plus,
+    Minus,
+    Star,
+    Slash,
+    Equal,
+    DoubleEqual,
+    NotEqual,
+    Modulo,
+
+    //literals
+    Identifier,
+    IntLiteral,
+    StringLiteral,
+    FloatLiteral,
+    DoubleLiteral,
+
+}
