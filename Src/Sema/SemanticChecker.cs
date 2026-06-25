1c9825aaa4395faa4eba7ca3c062420266af0db2
funcieqDEV <166367015+funcieqDEV@users.noreply.github.com>
2026-06-25 20:06:54 +0200

    feat: add semantic checker(kindof)

--- Src/Sema/SemanticChecker.cs
+++ Src/Sema/SemanticChecker.cs
@@ -3,5 +3,9 @@ using Src.AST;
 
 public class SemanticChecker
 {
-    
+    //im too lazy to implement this rn, so ill just return the root node for now
+    public RootNode Check(RootNode root)
+    {
+        return root;
+    }
 }
